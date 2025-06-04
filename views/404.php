3df5b1ec44d0c0bb6ad693baf2361bd3ded05849
taha1831 <34244428+taha1831@users.noreply.github.com>
2025-06-04 18:16:50 +0330

    Add files via upload

---
+++ views/404.php
@@ -0,0 +1,7 @@
+<div class="container mt-5">
+    <div class="alert alert-warning">
+        <h2>Page Not Found</h2>
+        <p>The requested page could not be found.</p>
+        <a href="index.php?page=home" class="btn btn-primary">Return Home</a>
+    </div>
+</div>
